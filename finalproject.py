f9b61f94218301036496a7c24a7bba50d4979cdd
mk12345g <42977570+mk12345g@users.noreply.github.com>
2023-07-28 13:55:11 -0400

    Update finalproject.py

--- finalproject.py
+++ finalproject.py
@@ -3,9 +3,8 @@ from jetson_utils import videoSource, videoOutput
 
 net = imageNet(model="resnet18.onnx", labels="labels.txt")
 camera = videoSource("csi://0")      # '/dev/video0' for V4L2
-display = videoOutput("display://0") # 'my_video.mp4' for file
 
-while display.IsStreaming():
+while True:
     img = camera.Capture()
 
     if img is None: # capture timeout
@@ -13,5 +12,4 @@ while display.IsStreaming():
 
     detections = net.Detect(img)
     
-    display.Render(img)
-    display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+   
