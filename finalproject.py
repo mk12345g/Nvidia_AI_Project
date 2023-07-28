c9013b0cc537c331924d76ca5df501b9e2f816fa
mk12345g <42977570+mk12345g@users.noreply.github.com>
2023-07-28 13:57:42 -0400

    Update finalproject.py

--- finalproject.py
+++ finalproject.py
@@ -2,7 +2,7 @@ from jetson_inference import imageNet
 from jetson_utils import videoSource, videoOutput
 
 net = imageNet(model="resnet18.onnx", labels="labels.txt", input_blob="input_0", output_blob="output_0") 
-camera = videoSource("csi://0")      # '/dev/video0' for V4L2
+camera = videoSource("/dev/video0")      # '/dev/video0' for V4L2
 
 while True:
     img = camera.Capture()
